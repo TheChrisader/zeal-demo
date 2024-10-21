ead62478e62cbff5123f6ca1a8338337372a3eaa
Christopher <IkeoChris@protonmail.com>
2024-10-21 15:03:28 +0100

    qs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import CopyPlugin from "copy-webpack-plugin";
 import path from "path";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
