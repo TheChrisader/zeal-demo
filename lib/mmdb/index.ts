6ac6571943cf28fe89bfbcbcacc0a0eb6605d3d3
Christopher <IkeoChris@protonmail.com>
2024-10-21 15:12:10 +0100

    dvd

--- lib/mmdb/index.ts
+++ lib/mmdb/index.ts
@@ -1,12 +1,13 @@
 import * as MMDB from "mmdb-lib";
 import { promises as fs } from "fs";
+import path from "path";
 
 export const getMMDB = async () => {
   //   const mmdbLocation = await fs.readFile(
   //     process.cwd() + "/public/GeoLite2-Country.mmdb",
   //   );
   const mmdbLocation = await fs.readFile(
-    process.cwd() + ".next/server/data/GeoLite2-Country.mmdb",
+    path.join(process.cwd(), "data/GeoLite2-Country.mmdb"),
   );
 
   return new MMDB.Reader<MMDB.CountryResponse>(mmdbLocation);
