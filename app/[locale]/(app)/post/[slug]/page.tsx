90bf43646d749342071e3d6d14dc7dae2c08e10d
Christopher <IkeoChris@protonmail.com>
2025-01-11 12:06:44 +0100

    cnfdr

--- app/[locale]/(app)/post/[slug]/page.tsx
+++ app/[locale]/(app)/post/[slug]/page.tsx
@@ -207,7 +207,7 @@ export default async function PostPage({
       </div>
 
       <div
-        className="rounded-[20px] p-1 [&_a]:text-blue-500 [&_figcaption]:text-center [&_figcaption]:text-sm [&_figcaption]:font-bold [&_figure>img]:mb-2 [&_figure>img]:mt-4 [&_figure>img]:max-h-[350px] [&_figure>img]:rounded-md [&_figure>p]:text-black [&_figure]:mb-7 [&_figure]:flex [&_figure]:w-full [&_figure]:flex-col [&_figure]:items-center [&_img]:mx-auto [&_img]:block [&_img]:max-h-[350px] [&_img]:w-1/2 [&_img]:rounded-md [&_img]:object-cover [&_img]:object-center [&_p]:mb-4 [&_p]:max-w-[100vw] [&_p]:text-base [&_p]:font-normal [&_p]:text-[#696969]"
+        className="rounded-[20px] p-1 [&_a]:text-blue-500 [&_figcaption]:text-center [&_figcaption]:text-sm [&_figcaption]:font-bold [&_figure>img]:mb-2 [&_figure>img]:mt-4 [&_figure>img]:max-h-[350px] [&_figure>img]:rounded-md [&_figure>p]:text-black [&_figure]:mb-7 [&_figure]:flex [&_figure]:w-full [&_figure]:flex-col [&_figure]:items-center [&_img]:mx-auto [&_img]:block [&_img]:max-h-[350px] [&_img]:w-1/2 [&_img]:rounded-md [&_img]:object-cover [&_img]:object-center [&_p]:mb-4 [&_p]:max-w-[100vw] [&_p]:text-base [&_p]:font-normal [&_p]:leading-7 [&_p]:text-[#0C0C0C]"
         dangerouslySetInnerHTML={{ __html: post.content }}
       />
       <span id="data-article-end" />
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata, Viewport } from "next";
 import dynamic from "next/dynamic";
 import { Poppins } from "next/font/google";
+import localFont from "next/font/local";
 import "../globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
@@ -30,6 +31,37 @@ const CookieConsent = dynamic(
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800"],
+});
+
+const cnnSansDisplay = localFont({
+  src: [
+    {
+      path: "../fonts/CNNSansDisplay-Regular.ttf",
+      weight: "400",
+      // style: "normal",
+    },
+    {
+      path: "../fonts/CNNSansDisplay-Medium.ttf",
+      weight: "500",
+      // style: "normal",
+    },
+    {
+      path: "../fonts/CNNSansDisplay-Bold.ttf",
+      weight: "600",
+      // style: "normal",
+    },
+    {
+      path: "../fonts/CNNSansDisplay-Heavy.ttf",
+      weight: "700",
+      // style: "normal",
+    },
+    {
+      path: "../fonts/CNNSansDisplay-Black.ttf",
+      weight: "800",
+      // style: "normal",
+    },
+  ],
+  variable: "--font-cnn-sans-display",
 });
 
 export const metadata: Metadata = {
@@ -72,7 +104,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
-      <body className={`${poppins.className}`}>
+      <body className={`${cnnSansDisplay.className}`}>
         {/* <Suspense> */}
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider defaultTheme="system" attribute="class" enableSystem>
