dceb9c737dde696acdb8a2f232bfa843cc79365f
Christopher <IkeoChris@protonmail.com>
2025-06-16 04:30:56 +0100

    gh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,24 @@
+console.log(__dirname, "!!!!!1");
 module.exports = {
   apps: [
     {
       name: "zeal-news",
-      script: "node_modules/.bin/next",
+      //   script: `./node_modules/next/dist/bin/next`,
+      //   script: "./node_modules/.bin/next",
+      script: "next",
+      //   script: "npm",
       args: "start",
-      cwd: "./",
+      exec_interpreter: "bash",
+      //   args: "./node_modules/next/dist/bin/next start",
+      cwd: ".",
+
       // Tell PM2 to fork the app for each available CPU core
       instances: "max",
       exec_mode: "cluster",
       // Optional: auto-restart if the app crashes
-      autorestart: true,
+      autorestart: false,
       watch: false,
-      max_memory_restart: "1G", // Optional: restart if it exceeds 1GB memory
+      max_memory_restart: "1G", // Optional: restart if it exceeds 1GB memory,
     },
   ],
 };
