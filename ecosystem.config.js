4934338308f4d9900397330a8d99a912a4a6b0f4
Christopher <IkeoChris@protonmail.com>
2025-06-16 05:47:54 +0100

    lye

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,11 @@ module.exports = {
     {
       name: "zeal-news",
       script: `./node_modules/next/dist/bin/next`,
-      //   script: "./node_modules/.bin/next",
-      //   script: "next",
-      //   script: "npm",
-      args: "start ./",
-      //   exec_interpreter: "bash",
-      //   args: "./node_modules/next/dist/bin/next start",
-      cwd: "./",
+      args: "start",
 
-      // Tell PM2 to fork the app for each available CPU core
       instances: "max",
       exec_mode: "cluster",
-      // Optional: auto-restart if the app crashes
-      autorestart: false,
+      autorestart: true,
       watch: false,
       max_memory_restart: "1G", // Optional: restart if it exceeds 1GB memory,
     },
