1c9875532fad1c236ef992b7bd4285f2cbb55ea8
Christopher <IkeoChris@protonmail.com>
2025-06-17 12:04:00 +0100

    gb

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       exec_mode: "cluster",
       autorestart: true,
       watch: false,
-      max_memory_restart: "1.5G", // Optional: restart if it exceeds 1GB memory,
+      max_memory_restart: "2000M", // Optional: restart if it exceeds 1GB memory,
     },
   ],
 };
