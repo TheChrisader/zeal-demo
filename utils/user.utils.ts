8ead60966a599c873f261b885b04635ea4c0596b
Christopher <IkeoChris@protonmail.com>
2025-07-18 16:43:47 +0100

    bkoydb

--- utils/user.utils.ts
+++ utils/user.utils.ts
@@ -1,13 +1,13 @@
 import { User } from "lucia";
 
 export const checkUserWriterStatus = (user: User) => {
-  return user.role === "writer" || user.role === "freelance_writer";
+  return user?.role === "writer" || user?.role === "freelance_writer";
 };
 
 export const checkIsUserFreelance = (user: User) => {
-  return user.role === "freelance_writer";
+  return user?.role === "freelance_writer";
 };
 
 export const checkUserUpgradeStatus = (user: User) => {
-  return user.upgrade_pending;
+  return user?.upgrade_pending;
 };
