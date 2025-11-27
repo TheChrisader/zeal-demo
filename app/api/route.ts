9441b0e30a3a40d2492b0a4dbc6442c40292a800
Christopher <IkeoChris@protonmail.com>
2025-11-27 12:45:08 +0100

    sgh

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,22 +1,21 @@
 import { NextResponse } from "next/server";
-import PostModel from "@/database/post/post.model";
-import { SlugGenerator } from "@/lib/slug";
+import SubscriberModel from "@/database/subscriber/subscriber.model";
+import { isMongooseDuplicateKeyError } from "@/utils/mongoose.utils";
 
 export async function POST() {
   try {
-    const generator = new SlugGenerator();
-    const posts = await PostModel.find();
-    const updates = posts.map((post) => {
-      post.slug = generator.generate(post.title);
-      return post.save();
+    await SubscriberModel.create({
+      email_address: "florenceidimone@gmail.com",
     });
-    await Promise.all(updates);
 
     return NextResponse.json({
-      message: `${posts.length} posts updated`,
+      message: `posts updated`,
     });
   } catch (err) {
-    console.log(err);
-    NextResponse.json({ err });
+    if (isMongooseDuplicateKeyError(err)) {
+      return NextResponse.json({ message: "You're gonna make this right." });
+    }
+
+    return NextResponse.json({ err });
   }
 }
