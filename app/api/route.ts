3f6da5e1c21ec8d8f764e992ae18c83cc0ef7220
Christopher <IkeoChris@protonmail.com>
2025-11-27 12:46:32 +0100

    sdf

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,20 +1,14 @@
 import { NextResponse } from "next/server";
-import SubscriberModel from "@/database/subscriber/subscriber.model";
-import { isMongooseDuplicateKeyError } from "@/utils/mongoose.utils";
 
 export async function POST() {
   try {
-    await SubscriberModel.create({
-      email_address: "florenceidimone@gmail.com",
-    });
-
     return NextResponse.json({
       message: `posts updated`,
     });
   } catch (err) {
-    if (isMongooseDuplicateKeyError(err)) {
-      return NextResponse.json({ message: "You're gonna make this right." });
-    }
+    // if (isMongooseDuplicateKeyError(err)) {
+    //   return NextResponse.json({ message: "You're gonna make this right." });
+    // }
 
     return NextResponse.json({ err });
   }
